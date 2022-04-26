acd59d2fe7cf7cb553c247c5139984a31c05305b
namenega <mene.nath@protonmail.com>
2022-04-26 16:24:51 +0200

    Namenega: update Docs Readme

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,7 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  // create application
   const app = await NestFactory.create(AppModule);
+
+  //launch on port 3000
   await app.listen(3000);
 }
 bootstrap();
