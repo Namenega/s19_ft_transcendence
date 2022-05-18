0603fc0a05fcdfd24e27a39b85a72e83ec3c35d7
namenega <mene.nath@protonmail.com>
2022-05-18 11:53:02 +0200

    Namenega: friends service start

--- backend/src/friends/friends.service.ts
+++ backend/src/friends/friends.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CreateFriendsDto } from './dto/createFriends.dto';
 import { FriendsEntity } from './entities/friends.entity';
 
 @Injectable()
@@ -10,5 +11,18 @@ export class FriendsService {
 		private FriendsRepo: Repository<FriendsEntity>
 	) {}
 
-	async create() {}
+	async create(CreateFriendsDto: CreateFriendsDto): Promise<void> {
+		await this.FriendsRepo.save(CreateFriendsDto);
+	}
+
+	async findAll(): Promise<FriendsEntity[]> {
+		return await this.FriendsRepo.find();
+	}
+
+	async findUserFriends(login: string): Promise<FriendsEntity[]> {
+		return await this.FriendsRepo.find({
+			relations: ['user'],
+			where: { user: {login: login}}
+		});
+	}
 }
