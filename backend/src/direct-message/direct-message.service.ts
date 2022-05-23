1eb82137334f894a88f7e8a641e1f710be164f52
namenega <mene.nath@protonmail.com>
2022-05-23 15:14:03 +0200

    Namenega: add direct-message

--- backend/src/direct-message/direct-message.service.ts
+++ backend/src/direct-message/direct-message.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { DirectMessageEntity } from './entities/directMessage.entity';
+import { DirectMessageListEntity } from './entities/directMessageList.entity';
 
 @Injectable()
-export class DirectMessageService {}
+export class DirectMessageService {
+	constructor(
+		@InjectRepository(DirectMessageEntity)
+		private dm: Repository<DirectMessageEntity>,
+		@InjectRepository(DirectMessageListEntity)
+		private dmList: Repository<DirectMessageListEntity>
+	) {}
+}
