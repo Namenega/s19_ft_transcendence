f1bbc969dea7b2bbf07672d056e3608fdcee0eab
Namenega <mene.nath@protonmail.com>
2022-05-30 10:12:14 -0400

    Namenega: profile css

--- work_chez_pyg/front/src/pages/Profil.js
+++ work_chez_pyg/front/src/pages/Profil.js
@@ -1,9 +1,15 @@
 const Profil = () => {
 
     return (
-        <div className="game-main-ctn">
-            <div className="game-ctn">
-            </div>
+        <div className="profile-main-ctn">
+            <div className="profile-banner-ctn">
+		<div className="profile-banner-avatar-ctn">AVATAR</div>
+		<div className="profile-banner-statistics-ctn">STATISTICS</div>
+	    </div>
+	    <div className="profile-history-achievements-ctn">
+		    <div className="profile-history-ctn">Match History</div>
+		    <div className="profile-achievements-ctn">Achievements</div>
+	    </div>
         </div>
     )
 }
