20207c15d58ae82cfb893c30438b6e3a1566e765
Namenega <mene.nath@protonmail.com>
2022-05-30 14:07:19 -0400

    Namenega: css Game

--- work_chez_pyg/front/src/pages/Game.js
+++ work_chez_pyg/front/src/pages/Game.js
@@ -2,13 +2,19 @@ const Game = () => {
 
     return (
         <div className="game-main-ctn">
-            <div className="game-banner-ctn">
-        <div className="game-banner-live-watch-ctn">Live watch</div>
-		<div className="game-banner-start-ctn">Start</div>
-        <div className="game-banner-custom-option-ctn">Custom option</div>
-        </div>
-        <div className="game-game-frame-ctn">
-        </div>
+        	<div className="game-banner-ctn">
+        		<div className="game-banner-live-watch-ctn">
+	    			<div className="game-button-text">Live watch</div>
+	    		</div>
+			<div className="game-banner-start-ctn">
+	    			<div className="game-button-text">Start</div>
+	    		</div>
+        		<div className="game-banner-custom-option-ctn">
+	    			<div className="game-button-text">Custom options</div>
+	    		</div>
+        	</div>
+        	<div className="game-game-frame-ctn">
+		</div>
         </div>
     )
 }
