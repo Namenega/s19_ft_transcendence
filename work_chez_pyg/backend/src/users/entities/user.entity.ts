f33e2ac0fdf739c96447650446b6433db52ebf71
Pyg Argus <mene.nath@protonmail.com>
2022-06-01 15:30:57 +0200

    Namenega: api/user

--- work_chez_pyg/backend/src/users/entities/user.entity.ts
+++ work_chez_pyg/backend/src/users/entities/user.entity.ts
@@ -13,99 +13,98 @@ import { Type } from 'class-transformer';
 called id */
 @Entity()
 export class UserEntity {
-    /* Creating a primary key for the table. */
+	/* Creating a primary key for the table. */
 	@PrimaryGeneratedColumn()
-    id: number;
+	id: number;
 
-    /* Creating a column in the database called name, and it is unique, not empty,
+	/* Creating a column in the database called name, and it is unique, not empty,
 	and a string. */
 	@Column({unique: true})
-    @IsNotEmpty()
-    @IsString()
-    name: string;
+	@IsNotEmpty()
+	@IsString()
+	name: string;
 
-    /* Creating a column in the database called login, and it is unique, not empty,
+	/* Creating a column in the database called login, and it is unique, not empty,
 	and a string. */
 	@Column({unique: true})
-    @IsNotEmpty()
-    @IsString()
-    login: string;
+	@IsNotEmpty()
+	@IsString()
+	login: string;
 
-    /* Creating a column in the database called password, and it is a string. */
+	/* Creating a column in the database called password, and it is a string. */
 	@Column()
-    @IsString()
-    password: string
+	@IsString()
+	password: string
 
-    /* Creating a column in the database called avatar, and it is a string. */
+	/* Creating a column in the database called avatar, and it is a string. */
 	@Column()
-    @IsString()
-    avatar: string;
+	@IsString()
+	avatar: string;
 
-    /* Creating a column in the database called has2FA, and it is a boolean. */
+	/* Creating a column in the database called has2FA, and it is a boolean. */
 	@Column()
-    @IsBoolean()
-    has2FA: boolean;
+	@IsBoolean()
+	has2FA: boolean;
 
-    /* Creating a column in the database called secret2FA, and it is a string. */
+	/* Creating a column in the database called secret2FA, and it is a string. */
 	@Column()
-    @IsString()
-    secret2FA: string;
+	@IsString()
+	secret2FA: string;
 
-    /* A validation for the status column. */
+	/* A validation for the status column. */
 	@Column()
-    @IsIn(["Online", "Offline", "In a game", "Searching a game"])
-    status: string;
+	@IsIn(["Online", "Offline", "In a game", "Searching a game"])
+	status: string;
 
-    /* Creating a column in the database called numberOfWin, and it is an integer. */
+	/* Creating a column in the database called numberOfWin, and it is an integer. */
 	@Column()
-    @IsInt()
-    @Min(0)
-    numberOfWin: number;
+	@IsInt()
+	@Min(0)
+	numberOfWin: number;
 
-    /* Creating a column in the database called numberOfLoss, and it is an integer. */
+	/* Creating a column in the database called numberOfLoss, and it is an integer. */
 	@Column()
-    @IsInt()
-    @Min(0)
-    numberOfLoss: number;
+	@IsInt()
+	@Min(0)
+	numberOfLoss: number;
 
-   /* Creating a one to many relationship between the UserEntity and the
-   MatchHistoryEntity. */
-    @OneToMany(type => MatchHistoryEntity, MatchHistoryEntity => MatchHistoryEntity.user)
-    @IsOptional()
-    @IsArray()
-    @ValidateNested({ each: true })
-    @Type(() => MatchHistoryEntity)
-    matchHistory: MatchHistoryEntity[]
+	/* Creating a one to many relationship between the UserEntity and the
+	MatchHistoryEntity. */
+	@OneToMany(type => MatchHistoryEntity, MatchHistoryEntity => MatchHistoryEntity.user)
+	@IsOptional()
+	@IsArray()
+	@ValidateNested({ each: true })
+	@Type(() => MatchHistoryEntity)
+	matchHistory: MatchHistoryEntity[]
 
-    /* Creating a one to many relationship between the UserEntity and the
-    FriendsEntity. */
-    @OneToMany(type => FriendsEntity, FriendsEntity => FriendsEntity.user)
-    @IsOptional()
-    @IsArray()
-    @ValidateNested({ each: true })
-    @Type(() => FriendsEntity)
-    friends: FriendsEntity[]
+	/* Creating a one to many relationship between the UserEntity and the
+	FriendsEntity. */
+	@OneToMany(type => FriendsEntity, FriendsEntity => FriendsEntity.user)
+	@IsOptional()
+	@IsArray()
+	@ValidateNested({ each: true })
+	@Type(() => FriendsEntity)
+	friends: FriendsEntity[]
 
-    @ManyToMany(type => DirectMessageEntity, DirectMessageEntity => DirectMessageEntity.users)
-    @IsOptional()
-    @IsArray()
-    @ValidateNested({ each: true })
-    @Type(() => DirectMessageEntity)
-    directMessage: DirectMessageEntity[]
+	@ManyToMany(type => DirectMessageEntity, DirectMessageEntity => DirectMessageEntity.users)
+	@IsOptional()
+	@IsArray()
+	@ValidateNested({ each: true })
+	@Type(() => DirectMessageEntity)
+	directMessage: DirectMessageEntity[]
 
-    @ManyToMany(type => ChannelsEntity, ChannelsEntity => ChannelsEntity.users)
-    @JoinTable()
-    @IsOptional()
-    @IsArray()
-    @ValidateNested({ each: true })
-    @Type(() => ChannelsEntity)
-    channels: ChannelsEntity[]
+	@ManyToMany(type => ChannelsEntity, ChannelsEntity => ChannelsEntity.users)
+	@JoinTable()
+	@IsOptional()
+	@IsArray()
+	@ValidateNested({ each: true })
+	@Type(() => ChannelsEntity)
+	channels: ChannelsEntity[]
 
-    /* A column in the database called latestTimeOnline, and it is a string. */
+	/* A column in the database called latestTimeOnline, and it is a string. */
 	@Column()
-    @IsString()
-    @IsNotEmpty()
-    @IsNumberString()
-    latestTimeOnline: string
-    
+	@IsString()
+	@IsNotEmpty()
+	@IsNumberString()
+	latestTimeOnline: string
 }
