7fb112970c24bbac54d3b44c9e83cda8acc02258
namenega <mene.nath@protonmail.com>
2022-06-09 17:16:51 +0200

    Namenega: front/src/api/updateFriendsDto

--- work_chez_pyg/front/src/api/friends/dto/updateFriends.dto.ts
+++ work_chez_pyg/front/src/api/friends/dto/updateFriends.dto.ts
@@ -1,3 +1,3 @@
 import { FriendsDto } from "./friends.dto"; //import { CreateFriendsDto } from "./createFriends.dto"; ?
 
-export type UpdateFriendDto = Partial<FriendsDto>; //export type UpdateFriendsDto = Partial<CreateFriendsDto>;?
+export type UpdateFriendsDto = Partial<FriendsDto>; //export type UpdateFriendsDto = Partial<CreateFriendsDto>;?
