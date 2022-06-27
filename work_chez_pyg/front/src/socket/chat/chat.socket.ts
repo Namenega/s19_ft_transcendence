fef334f4ebdd483085a4f02205b5a1828baa1ee7
Namenega <mene.nath@protonmail.com>
2022-06-27 12:57:05 +0200

    front/ chat socket

---
+++ work_chez_pyg/front/src/socket/chat/chat.socket.ts
@@ -0,0 +1,25 @@
+#import { io } from "socket.io-client"
+
+export const const connect: () => any = () => {
+	return io("http://localhost:80/chat");
+}
+
+export const listen: (socket: any, callbackFunc: (reponse: string) => void) => void = (socket, callbackFunc) => {
+	socket.on('message', callbackFunc);
+}
+
+export const disconnect: (socket: any) => void = (socket) => {
+	socket.disconnect();
+}
+
+export const joinRoom: (socket: any, room: string) => void = (socket, room) => {
+	socket.emit("joinRoom", room);
+}
+
+export const leaveRoom: (socket: any, room: string) => void = (socket, room) => {
+	socket.emit("leaverRoom", room);
+}
+
+export const send: (socket: any, message: {room: string, content: string}) => void = (socket, message) => {
+	socket.emit("message", message);
+}
